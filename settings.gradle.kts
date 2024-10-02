7e93b02d4b43ca09207f03e195f719f8d6476704
powercas_gamer <cas@mizule.dev>
2024-10-02 21:37:03 +0000

    buildscript

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,18 @@
 pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        maven("https://repo.stellardrift.ca/repository/snapshots/")
-        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-        maven("https://repo.mizule.dev/releases")
-        maven("https://repo.mizule.dev/snapshots")
-        maven("https://repo.jpenilla.xyz/snapshots")
-    }
+  repositories {
+    mavenCentral()
+    gradlePluginPortal()
+    maven("https://repo.stellardrift.ca/repository/snapshots/")
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://repo.mizule.dev/releases")
+    maven("https://repo.mizule.dev/snapshots")
+    maven("https://repo.jpenilla.xyz/snapshots")
+//    mavenLocal()
+  }
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
@@ -25,6 +26,6 @@ sequenceOf(
     "paper",
     "velocity",
 ).forEach {
-    include("mserverlinks-$it")
-    project(":mserverlinks-$it").projectDir = file(it)
+  include("mserverlinks-$it")
+  project(":mserverlinks-$it").projectDir = file(it)
 }
