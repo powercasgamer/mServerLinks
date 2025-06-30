69cbbd0198947a3a22476d1e0d190e0e947191db
powercas_gamer <cas@mizule.dev>
2025-06-30 15:28:11 +0200

    it's 2025

--- core/src/main/java/dev/mizule/mserverlinks/core/util/ClassUtil.java
+++ core/src/main/java/dev/mizule/mserverlinks/core/util/ClassUtil.java
@@ -1,8 +1,8 @@
 /*
  * This file is part of mServerLinks, licensed under the MIT License.
  *
- * Copyright (c) 2024 powercas_gamer
- * Copyright (c) 2024 contributors
+ * Copyright (c) 2024-2025 powercas_gamer
+ * Copyright (c) 2024-2025 contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
